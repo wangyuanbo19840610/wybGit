5157638e07766a87385369afbf0a037e12fb04b3
wangyuanbo <wangyuanbo1984@126.com>
2019-10-19 21:22:47 +0800

    modify

--- flask-project/restapi/Common_app.py
+++ flask-project/restapi/Common_app.py
@@ -1,9 +1,10 @@
-from flask import Flask, current_app
+from flask import Flask
 
 
 class CommonApp(object):
     def __new__(cls, **kwargs):
         cls.app = Flask(__name__)
+        """ 配置redis """
         cls.app.config['REDIS_HOST'] = "127.0.0.1"  # redis数据库地址
         cls.app.config['REDIS_PORT'] = 6379  # redis 端口号
         cls.app.config['REDIS_DB'] = 0  # 数据库名
