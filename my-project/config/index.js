cb82941db6075311fbd3168723e0f8e81485c50e
wangyuanbo <wangyuanbo1984@126.com>
2019-12-28 21:53:04 +0800

    modify

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -11,10 +11,10 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api/': {
-        target: 'http://127.0.0.1:5000',  //目标接口域名
+        target: 'http://192.168.0.101',  //目标接口域名
         changeOrigin: true,  //是否跨域
         pathRewrite: {
-          '^/api': ''   //重写接口
+          '^/api': '/api'   //重写接口
         }
       }
     },
