b7e776483acb661837145816f4d02aa888f694bd
wangyuanbo <wangyuanbo1984@126.com>
2020-02-13 14:22:22 +0800

    请求GeoServer图层

--- my-project/src/components/mainpage.vue
+++ my-project/src/components/mainpage.vue
@@ -66,15 +66,34 @@ export default{
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.OSM()
+            source: new ol.source.OSM(),
+            visible: true
+          }),
+          new ol.layer.Tile({
+            source:new ol.source.TileWMS({
+              url:'http://localhost:8080/geoserver/topp/wms',//'https://ahocevar.com/geoserver/wms',
+              params:{
+                'LAYERS':'topp:tasmania_roads',
+              },
+              serverType:'geoserver',
+              transition:5000
+            })
           })
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([103.73,36.03]),
+          center:ol.proj.fromLonLat([147.59583, -42.04926]),//[-70.96065,40.78431], ,//ol.proj.fromLonLat([103.73,36.03]),
           zoom: 8,
-          minZoom:5,
-          maxZoom:12
-        })
+          // minZoom:5,
+          // maxZoom:12
+        }),
+        // 新增代码
+        controls: ol.control.defaults().extend([    // 往地图增加滑块缩放控件
+            new ol.control.ZoomSlider(),
+            new ol.control.OverviewMap({        // 实例化一个OverviewMap类的对象，并加入到地图中
+                collapsed: false
+            }),
+            new ol.control.ScaleLine() 
+        ]),
       });
     }
   }
